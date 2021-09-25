1e363538a6ec7353a4de251b82fb47853eddab56
iris <iris@serv.pink>
2021-09-25 09:58:26 -0400

    deploy.sh: support deploying to different hosts

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+target="${1-skitter}"
+echo "pushing to host: $target"
+
 mypy skitter_worker.py skitter_api.py
 
-rsync -aPv ../skitter_api/ skitter:skitter_api
-rsync -aPv ../python-oil ../python-weaver skitter:
-rsync -aPv --no-owner --no-group ./etc/ root@skitter:/etc/
+rsync -aPv ../skitter_api/ skitter@${target}:skitter_api
+rsync -aPv ../python-oil ../python-weaver skitter@${target}:
+rsync -aPv --no-owner --no-group ./etc/ root@${target}:/etc/
+rsync -aPv --no-owner --no-group ./var/ root@${target}:/var/
 
-ssh skitter './skitter_api/setup_skitter_env.sh'
+ssh skitter@${target} './skitter_api/setup_skitter_env.sh'
 
