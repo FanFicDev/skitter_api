4f304982deb40d4faa113d9b4a996c48af3898c7
iris <iris@serv.pink>
2021-12-30 10:12:08 -0500

    skitter_api.v0_crawl: ensure we have a new response to return
    This removes the v0_cache usage from v0_crawl, and ensures that the
    response we're attempting to return was actually crawled _after_ we got
    the request to do so. This fixes the rare case where someone is munging
    directly with the db and makes RemoteWebScraper.scrape return early
    without an exception.

--- skitter_api.py
+++ skitter_api.py
@@ -4,6 +4,7 @@ from typing import (
 	)
 import os
 import os.path
+import time
 import traceback
 import threading
 from enum import IntEnum
@@ -40,6 +41,15 @@ def v0_status() -> ResponseReturnValue:
 	return make_response({'err':0,'status':'ok',
 		'pid':os.getpid(),'tident':threading.get_ident(),})
 
+def make_response_web(w: Optional[Web]) -> ResponseReturnValue:
+	if w is None or w.response is None or w.created is None:
+		return page_not_found(NotFound())
+	fres = make_response(w.response)
+	fres.headers['X-Weaver-Id'] = str(w.id) + '_' + NODE_NAME
+	fres.headers['X-Weaver-Created'] = str(int(w.created//1000))
+	fres.headers['X-Weaver-Url'] = str(w.url)
+	return fres
+
 def get_request_value(key: str, dflt: Optional[str]) -> Optional[str]:
 	val = request.form.get(key, None)
 	if val is None:
@@ -71,13 +81,9 @@ def v0_cache() -> ResponseReturnValue:
 
 	if latest is None or latest.response is None or latest.created is None:
 		print(f'v0_cache: {q=}, {u=}: not found')
-		return page_not_found(NotFound())
-	print(f'v0_cache: {q=}, {u=}: found: len: {len(latest.response)}, url: {latest.url}, id: {latest.id}, created: {latest.created}')
-	fres = make_response(latest.response)
-	fres.headers['X-Weaver-Id'] = str(latest.id) + '_' + NODE_NAME
-	fres.headers['X-Weaver-Created'] = str(int(latest.created//1000))
-	fres.headers['X-Weaver-Url'] = str(latest.url)
-	return fres
+	else:
+		print(f'v0_cache: {q=}, {u=}: found: len: {len(latest.response)}, url: {latest.url}, id: {latest.id}, created: {latest.created}')
+	return make_response_web(latest)
 
 @app.route('/v0/crawl', methods=['GET'])
 def v0_crawl() -> ResponseReturnValue:
@@ -93,11 +99,19 @@ def v0_crawl() -> ResponseReturnValue:
 	if not q.startswith('http://') and not q.startswith('https://'):
 		return page_not_found(NotFound())
 
+	ts = int(time.time()) - 1
 	db = oil.open()
 	scraper = RemoteWebScraper(db)
 	scraper.scrape(q)
-
-	return v0_cache()
+	latest = Web.latest(db, q, status=200)
+	if latest is None or latest.created is None:
+		print(f'v0_crawl: {q=}: error: no latest entry')
+		return make_response({'err':-500,'msg':'internal server error'}, 500)
+	lts = int(latest.created//1000)
+	if lts < ts:
+		print(f'v0_crawl: {q=}: error getting fresh crawl: {ts} >= {lts}')
+		return make_response({'err':-500,'msg':'internal server error'}, 500)
+	return make_response_web(latest)
 
 if __name__ == '__main__':
 	app.run(debug=True)
