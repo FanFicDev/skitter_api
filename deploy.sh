d9a58d583e853b077fac2ee16085625ed98ffcc4
iris <iris@serv.pink>
2021-12-30 10:18:53 -0500

    skitter_story_worker: handle kind 2 requests through FR2 instances

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ fi
 target="${1-skitter}"
 echo "pushing to host: $target"
 
-mypy skitter_worker.py skitter_api.py
+mypy skitter_worker.py skitter_story_worker.py skitter_api.py
 
 rsync -aPv --exclude venv ../skitter_api/ skitter@${target}:skitter_api
 rsync -aPv ../python-oil ../python-weaver skitter@${target}:
